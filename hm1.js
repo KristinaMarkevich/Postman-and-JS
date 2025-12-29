d8eb67935c53a9f132a9a4d91858e737ce544dd0
Kristina <christin.paint@gmail.com>
2025-12-29 23:49:25 +0300

    Update hm1.js

--- hm1.js
+++ hm1.js
@@ -7,92 +7,92 @@
 //3. Output item_1 to the console.
 
 
-4. Create the variable "item_2"
+//4. Create the variable "item_2"
 
 
-5. Assign the number 3 to the variable item_2.
+//5. Assign the number 3 to the variable item_2.
 
 
-6. Output item_2 to the console.
+//6. Output item_2 to the console.
 
 
-7. Create the variable "item_3"
+//7. Create the variable "item_3"
 
 
-8. Assign the addition of item_1 and item_2 to the variable item_3.
+//8. Assign the addition of item_1 and item_2 to the variable item_3.
 
 
-9. Output item_3 to the console.
+//9. Output item_3 to the console.
 
 
-10. Create the variable "item_4"
+//10. Create the variable "item_4"
 
 
-11. Assign the string "Yolochka" to the variable item_4.
+//11. Assign the string "Yolochka" to the variable item_4.
 
 
-12. Output item_4 to the console.
+//12. Output item_4 to the console.
 
 
-13. Output the addition of item_3 and item_4 to the console.
+//13. Output the addition of item_3 and item_4 to the console.
 
 
-14. Output the multiplication of item_3 and item_4 to the console.
+//14. Output the multiplication of item_3 and item_4 to the console.
 
 
-15. Create the variable "item_5"
+//15. Create the variable "item_5"
 
 
-16. Assign the variable item_3 to the variable item_5
+//16. Assign the variable item_3 to the variable item_5
 
 
-17. Create the variable item_6.
+//17. Create the variable item_6.
 
 
-18. Create the variable item_6_type
+//18. Create the variable item_6_type
 
 
-19. Assign the value 15 to the variable item_6
+//19. Assign the value 15 to the variable item_6
 
 
-20. Assign the type of the variable item_6 to the variable item_6_type
+//20. Assign the type of the variable item_6 to the variable item_6_type
 
 
-21. Print the data type of item_6 to the console as —— "item_6 == " item_6, "item_6_type == " item_6_type ——
+//21. Print the data type of item_6 to the console as —— "item_6 == " item_6, "item_6_type == " item_6_type ——
 
 
-22. Create the variable item_7 and convert item_6 to a String in it.
+//22. Create the variable item_7 and convert item_6 to a String in it.
 
 
-23. Create the variable item_7_type
+//23. Create the variable item_7_type
 
 
 
-24. Assign the variable item_7_type the variable type item_7
+//24. Assign the variable item_7_type the variable type item_7
 
 
-25. Print the item_7 data type to the console as follows: —— “item_7 == ” item_7, “item_7_type == ” item_7_type ——
+//25. Print the item_7 data type to the console as follows: —— “item_7 == ” item_7, “item_7_type == ” item_7_type ——
 
 
-26. Create the variable “age_1” and assign it the value 10
+//26. Create the variable “age_1” and assign it the value 10
 
 
-27. Create the variable “age_2” and assign it the value 18
+//27. Create the variable “age_2” and assign it the value 18
 
 
-28. Create the variable “age_3” and assign it the value 60
+//28. Create the variable “age_3” and assign it the value 60
 
 
-29. Create an if statement in which you will check the value of the variable age_1
+//29. Create an if statement in which you will check the value of the variable age_1
 
 
-30. If age_1 < age_2, print to the console “You don’t have access because your age is ” + age_1 + “ It’s less then 
+//30. If age_1 < age_2, print to the console “You don’t have access because your age is ” + age_1 + “ It’s less then 
 
 
-31. If age_1 >= age_2 and age_1 < age_3, print to the console “Welcome !”
+//31. If age_1 >= age_2 and age_1 < age_3, print to the console “Welcome !”
 
 
-32. If age_1 > age_3, output "Keep calm and watch Culture channel" to the console.
+//32. If age_1 > age_3, output "Keep calm and watch Culture channel" to the console.
 
 
 33. Otherwise, output "Technical work."
