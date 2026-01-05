f130f7a812d20821c16382d00db1fecd8e47976c
Kristina <christin.paint@gmail.com>
2026-01-05 16:08:36 +0300

    Update hm1.js

--- hm1.js
+++ hm1.js
@@ -1,100 +1,101 @@
 //1. Create the variable "item_1"
-
-
 //2. Assign the number 5 to the variable item_1.
-
+let item_1 = 5;
 
 //3. Output item_1 to the console.
-
+console.log(item_1);
 
 //4. Create the variable "item_2"
-
-
 //5. Assign the number 3 to the variable item_2.
-
+let item_2 = 3;
 
 //6. Output item_2 to the console.
-
+console.log(item_2);
 
 //7. Create the variable "item_3"
-
-
 //8. Assign the addition of item_1 and item_2 to the variable item_3.
-
+let item_3 = item_1 + item_2;
 
 //9. Output item_3 to the console.
-
+console.log(item_3);
 
 //10. Create the variable "item_4"
-
-
 //11. Assign the string "Yolochka" to the variable item_4.
-
+let item_4 = "Yolochka";
 
 //12. Output item_4 to the console.
-
+console.log(item_4);
 
 //13. Output the addition of item_3 and item_4 to the console.
-
+console.log(item_3 + item_4);
 
 //14. Output the multiplication of item_3 and item_4 to the console.
-
+console.log(item_3 * item_4);
 
 //15. Create the variable "item_5"
-
-
 //16. Assign the variable item_3 to the variable item_5
-
+let item_5 = item_3;
 
 //17. Create the variable item_6.
-
-
 //18. Create the variable item_6_type
-
-
 //19. Assign the value 15 to the variable item_6
-
+let item_6 = 15;
 
 //20. Assign the type of the variable item_6 to the variable item_6_type
-
+let item_6_type = typeof(item_6);
 
 //21. Print the data type of item_6 to the console as —— "item_6 == " item_6, "item_6_type == " item_6_type ——
-
+console.log("item_6 ==", item_6);
+console.log("item_6_type ==", item_6_type);
 
 //22. Create the variable item_7 and convert item_6 to a String in it.
-
+let item_7 = String(item_6);
 
 //23. Create the variable item_7_type
-
+let item_7_type = typeof(item_7);
 
 
 //24. Assign the variable item_7_type the variable type item_7
-
-
 //25. Print the item_7 data type to the console as follows: —— “item_7 == ” item_7, “item_7_type == ” item_7_type ——
-
+console.log("item_7 ==", item_7);
+console.log("item_7_type ==", item_7_type);
 
 //26. Create the variable “age_1” and assign it the value 10
-
+let age_1 = 10;
 
 //27. Create the variable “age_2” and assign it the value 18
-
+let age_2 = 18;
 
 //28. Create the variable “age_3” and assign it the value 60
-
+let age_3 = 60;
 
 //29. Create an if statement in which you will check the value of the variable age_1
-
+if(age_1 = 10){
+    console.log("Correct")
+}
+else {
+    console.log("Need attention")
+};
 
 //30. If age_1 < age_2, print to the console “You don’t have access because your age is ” + age_1 + “ It’s less then 
-
+if(age_1 < age_2) {
+    console.log("You don’t have access cause your age is " + age_1 + " It’s less then " + age_2)
+};
 
 //31. If age_1 >= age_2 and age_1 < age_3, print to the console “Welcome !”
-
+if(age_1 >=  age_2) {
+    console.log("Welcome!")
+}
+else if (age_1 <  age_3) {
+    console.log("Welcome!")
+};
 
 //32. If age_1 > age_3, output "Keep calm and watch Culture channel" to the console.
-
-
 //33. Otherwise, output "Technical work."
-
+if(age_1 > age_3) {
+    console.log("Keep calm and look Culture channel")
+}
+else {
+    console.log("Technical work")
+};
 
