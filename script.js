d83df0c673f124a4d8ae50874ad04072e30861c6
Kristina <christin.paint@gmail.com>
2026-01-07 19:45:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Write a validation script using functions.
+
+// 1. The script must accept a string as input.
+
+// 2. After validating the string, display a message in the console stating exactly what is wrong with the entered string.
+
+// 3. Minimum 5 characters per string.
+
+// 4. Maximum 64 characters per string.
+
+// 5. The string must contain letters.
+
+// 6. Must contain at least one uppercase letter.
+
+// 7. Must contain at least one number.
+
+// 8. Must contain at least one @ sign.
+
+// 9. The string must not be empty.
